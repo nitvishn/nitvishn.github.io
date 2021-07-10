cb760f25a46d0cd0f0bb5efc94a135d55cc033aa
Vishnu Nittoor <nitvishn@gmail.com>
2021-07-11 05:20:32 +0530

    trying to fix resume

---
+++ resume/html2latex.py
@@ -0,0 +1,39 @@
+# convert html document to LaTeX
+import lxml.html # http://lxml.de/lxmlhtml.html
+from lxml import etree
+from io import StringIO, BytesIO
+
+def html2latex(el): # fill in this function to catch and convert html tags
+    result = []
+    if el.text:
+        result.append(el.text)
+    for sel in el:
+        if False: # get info
+            print('tag',sel.tag)
+            print('text',sel.text)
+            print('tail',sel.tail)
+            print('attrib',sel.attrib)
+        if sel.tag in ["h1"]:
+            result.append('\hmchapter{%s}' % html2latex(sel))
+        elif sel.tag in ["td", "table"]:
+            result.append("<%s>" % sel.tag)
+            result.append(html2latex(sel))
+            result.append("</%s>" % sel.tag)
+        elif sel.tag in ["span"]:  #
+            for att in sel.attrib.keys():
+                if att =='style':
+                    if sel.attrib[att] == 'font-style:italic':
+                        result.append(r'\textit{%s}' % (html2latex(sel)))
+        else:
+            result.append(html2latex(sel))
+        if sel.tail:
+            result.append(sel.tail)
+    return "".join(result)
+
+def string2latex(html):
+    # must be unicode or lxml parse crashes
+    parser = etree.HTMLParser()
+    tree   = etree.parse(StringIO(html), parser) # expects a file, use StringIO for string
+    root = tree.getroot()
+    latex = html2latex(root)
+    return latex
--- resume/resume_generator.py
+++ resume/resume_generator.py
@@ -2,6 +2,7 @@ import os
 from jinja2 import Template
 from latex import build_pdf
 import json
+import html2latex
 
 def loadData(filename):
     with open('../src/shared/' + filename, 'r') as file:
@@ -13,6 +14,8 @@ research = loadData('research.json')
 work = loadData('work.json')
 projects = loadData('projects.json')
 organisations = loadData('organisations.json')
+for key in projects:
+    projects[key] = html2latex.string2Latex(projects[key])
 print(projects)
 
 txt = t.render(something="Yes", research=research, work=work, projects=projects, organisations=organisations)
